e9e458594ca2546c92c48c7e33b165845653f4c4
Abraham2748 <abraham_jack10@hotmail.com>
2025-01-28 19:54:42 -0500

    Delete unused import

--- projects/edition-2025-01/src/app/app.component.ts
+++ projects/edition-2025-01/src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingListNgrxComponent } from './shopping-list-ngrx/shopping-list-ngrx.component';
 
 @Component({
-    selector: 'app-root',
-    imports: [ShoppingListComponent, ShoppingListNgrxComponent],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css'
+  selector: 'app-root',
+  imports: [ShoppingListNgrxComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'edition-2025-01';
